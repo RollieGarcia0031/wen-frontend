a759879dca6125219789d33c78d9cb9206526b31
RollieGarcia0031 <garciarollie827@gmail.com>
2025-09-01 21:22:04 +0800

    fix professor search dialog for student, patch modal closing

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { useAuthContext } from '@/context/AuthContext';
-import { useState, useEffect, useRef, use } from 'react';
+import React, { useState, useEffect, useRef, use } from 'react';
 import { fetchBackend } from '@/lib/api';
 import { SearchProfessorResponse, SearchProfessorResponseDataItem } from '@/lib/response';
 
@@ -37,84 +37,126 @@ function SentAppointments(){
     <button  onClick={() => dialogRef.current?.showModal()}>New</button>  
     <h1>Sent Appointments</h1>
   </div>
-  <NewAppointmentDialog />
+  <NewAppointmentDialog
+    nameInput={nameInput}
+    setNameInput={setNameInput}
+    dialogRef={dialogRef}
+    setProfessors={setProfessors}
+    professors={professors}
+  />
   </>
   );
 
-  // dialog pop up for adding a new appointment and searching for professors
-  function NewAppointmentDialog(){
-    return(
-      <dialog ref={dialogRef}
-        className='open:sm:w-[30rem]  open:sm:h-[80dvh] open:w-full
-          open:flex open:flex-col open:justify-center open:items-center'
-      >
-        <div className='flex flex-row justify-end align-top items-end w-full'>
-          <button onClick={() => dialogRef.current?.close()}>❌</button>
-        </div>
-        <div className='flex-1 w-full flex flex-col justify-center items-center'>
-          <h1>New Appointment</h1>
-          <SearchPanel />
-          <SearchResult />
-        </div>
-      </dialog>
-
-    );
-  }
-  
-  /**
-   * search panel of user student, where the user can search for professors
-   * TODO: add more search options
-   */
-  function SearchPanel(){
-    return (
-      <div className='w-full flex flex-row justify-center items-center gap-2'>
-        <label>
-          🔎: 
-          <input type='text' className='ml-2'
-            onChange={(e) => setNameInput(e.target.value)}
-          />
-        </label>
-        <div className='flex-1'>
-          other options...
-        </div>
-        <button onClick={() => search()}>Search</button>
+}
+// dialog pop up for adding a new appointment and searching for professors
+function NewAppointmentDialog({nameInput, setNameInput, dialogRef, setProfessors, professors}:{
+  nameInput: string,
+  setNameInput: React.Dispatch<React.SetStateAction<string>>,
+  dialogRef: React.RefObject<HTMLDialogElement | null>  ,
+  setProfessors: React.Dispatch<React.SetStateAction<SearchProfessorResponseDataItem[]>>,
+  professors: SearchProfessorResponseDataItem[],
+  }){
+  return(
+    <dialog ref={dialogRef} onSubmit={(e) => e.preventDefault()}
+      className='open:sm:w-[30rem]  open:sm:h-[80dvh] open:w-full
+        open:flex open:flex-col open:justify-center open:items-center'
+    >
+      <div className='flex flex-row justify-end align-top items-end w-full'>
+        <button onClick={() => dialogRef.current?.close()}>❌</button>
       </div>
-    );
+      <div className='flex-1 w-full flex flex-col justify-center items-center'>
+        <h1>New Appointment</h1>
+        <SearchPanel 
+          nameInput={nameInput}
+          setNameInput={setNameInput}
+          setProfessors={setProfessors}
+        />
+        <SearchResult professors={professors}/>
+      </div>
+    </dialog>
 
-    // search function handling
-    async function search() {
-      const body = JSON.stringify({name: nameInput});
-  
-      const response: SearchProfessorResponse = await fetchBackend("search/professor", "POST", body);
-      
-      if(response.success){
-        const { data } = response;
-        if(data){
-          setProfessors(data);
-        }
+  );
+}
+
+/**
+ * search panel of user student, where the user can search for professors
+ * TODO: add more search options
+ */
+function SearchPanel({nameInput, setNameInput, setProfessors}: any){
+  return (
+    <div className='w-full flex flex-row justify-center items-center gap-2'
+    >
+      <label>
+        🔎: 
+        <input type='text' className='ml-2'
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value) }
+        />
+      </label>
+      <div className='flex-1'>
+        other options...
+      </div>
+      <button type='button' onClick={e=>search(e)}>Search</button>
+    </div>
+  );
+
+  // search function handling
+  async function search(e: any){
+    e.preventDefault();
+    
+    const body = JSON.stringify({name: nameInput});
+    
+    const response: SearchProfessorResponse = await fetchBackend("search/professor", "POST", body);
+    
+    console.log(response);
+    if(response.success){
+      const { data } = response;
+      if(data){
+        return setProfessors(data);
       }
-      console.log(response);
     }
   }
+}
 
   /**
    * the box that shows the search result of user student
    */
-  function SearchResult(){
-    return (
-      <div className='border-gray-500 border-2 border-solid m-4 w-[100%] rounded-md
-        flex-1
-      '>
-        {professors.map((professor) => <div key={professor.prof_id}>{professor.name}</div>)}
-        {professors.length === 0 && <h1>Search Result</h1>}
-      </div>
-    );
-  }
+function SearchResult({professors}: {professors: SearchProfessorResponseDataItem[]}){
+  return (
+    <div className='border-gray-500 border-2 border-solid m-4 w-[100%] rounded-md
+      flex-1
+    '>
+      {professors?.map((professor) => <SearchResultContainer
+        key={professor.prof_id}
+        id={professor.prof_id}
+        year={professor.year}
+        department={professor.department}
+      />)}
 
+      {professors?.length === 0 && <h1>Search Result</h1>}
+    </div>
+  );
+}
+
+function SearchResultContainer({id, year, department}:{
+  id?: number,
+  year?: number,
+  department?: string
+}){
+  return (
+    <div>
+      <p>id: {id}</p>
+      <p>year: {year}</p>
+      <p>department: {department}</p>
+    </div>
+  );
 }
 
 
 
+/**
+ * appointment panel rendered for professors
+ */
 function ReceivedAppointments(){
   return (
     <div>
