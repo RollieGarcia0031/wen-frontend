3d37eb66cf6314a3c5e06da651246831bedf77de
RollieGarcia0031 <garciarollie827@gmail.com>
2025-09-05 20:27:38 +0800

    add autofetching of prof availability

--- src/app/appointment/send/[id]/page.tsx
+++ src/app/appointment/send/[id]/page.tsx
@@ -1,16 +1,59 @@
 "use client";
 
-import {use} from 'react';
+import {use, useEffect, useState} from 'react';
+import { SearchAvailabilityResponseDataItem } from '@/lib/response';
+import { fetchBackend } from '@/lib/api';
+import { parse } from 'path';
 
 export default function SendAppointment({params}: {
   params : Promise<{id: number}>
 }) {
 
+  // id of professor in the users table
   const { id } = use(params);
   return (
-    <div>
+    <div
+      className='w-full h-full flex flex-col justify-center items-center'
+    >
       <h1>Send Appointment</h1>
-      <p>{id}</p>
+
+      <AvailabilityPanel
+        id={id}
+      />
+    </div>
+  );
+}
+
+function AvailabilityPanel({id}:{
+  id:number
+}){
+  const [availabilities, setAvailabilities] = useState<SearchAvailabilityResponseDataItem[]>([]);
+  
+  useEffect(()=>{
+    const body = {id: parseInt(id.toString())};
+
+    const fetchAvailabilities = async () =>  {
+      const response = await fetchBackend(
+        'search/availability',
+        'POST', JSON.stringify(body),
+        new Headers({"Content-Type": "application/json"})
+      );
+
+      if(response.success && response.data) {
+        console.log(response.data);
+        setAvailabilities(response.data);
+      } 
+    }
+
+    fetchAvailabilities().catch(console.error);
+  },[]);
+
+  return (
+    <div
+      className='border-gray-500 border-2 border-solid rounded-md p-4 w-5xl'
+    >
+        <p>Availability</p>
+
     </div>
   );
 }
