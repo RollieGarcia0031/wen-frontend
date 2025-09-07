8743aa97b966d01d08a1537bfeaba80e8bd13db5
RollieGarcia0031 <garciarollie827@gmail.com>
2025-09-07 12:41:44 +0800

    add dynamic toggle of design header per url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import { useAuthContext } from "@/context/AuthContext";
 import { useEffect, use } from "react";
+import { usePathname } from "next/navigation";
+import { IoHomeOutline } from "react-icons/io5";
+import { MdOutlineAttachEmail } from "react-icons/md";
 
 export default function Header(){
   const { role } = useAuthContext();
@@ -19,15 +22,41 @@ export default function Header(){
 }
 
 function StudentHeader(){
+  const pathname = usePathname().split("/");
+  const basePath = pathname[1];
+
+  const HeaderIconAnchorDesign = (requiredPath: string) =>{
+    return `${basePath === requiredPath ? "bg-primary" : ""}`;
+  }
+
   return (
-    <header>
-      <nav>
-        <div className="flex gap-4 flex-row">
-          <Link href="/">Home</Link>
-          <Link href="/appointment">Send Appointments</Link>
-          <div className='flex-1 text-right'>
-            <Link href="/login">Login</Link>
-          </div>
+    <header
+      className="flex flex-row justify-center items-center h-full p-2"
+    >
+      <nav className="w-full flex flex-row">
+        <div
+          className="flex gap-4 flex-row
+          justify-center items-center
+          *:flex-row *:flex *:justify-center *:items-center *:gap-2
+          [&_svg]:text-2xl [&_svg]:cursor-pointer [&_a]:duration-200
+          sm:[&_a]:p-2 sm:[&_a]:rounded-xl
+          "
+        
+        >
+          <Link href="/"
+            className={HeaderIconAnchorDesign("")}
+          >
+            <IoHomeOutline />
+          </Link>
+          <Link href="/appointment"
+            className={HeaderIconAnchorDesign("appointment")}
+          >
+            <MdOutlineAttachEmail />
+            Send Appointments
+          </Link>
+        </div>
+        <div className='flex-1 flex flex-row justify-end'>
+          <Link href="/login">Login</Link>
         </div>
       </nav>
     </header>
