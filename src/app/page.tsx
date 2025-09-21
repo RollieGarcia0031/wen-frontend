30b05d8dfeb0f97edf3d4f8092d1872413bea132
RollieGarcia0031 <garciarollie827@gmail.com>
2025-09-21 20:41:44 +0800

    add design for current latest appointment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import Link from "next/link";
 import { useAuthContext } from "@/context/AuthContext";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import Header from "@/components/Header";
+import { useState } from "react";
+import { fetchBackend } from "@/lib/api";
 
 export default function Home() {
-  const { role } = useAuthContext();
+  const { role, userName } = useAuthContext();
+  const [ latestAppointments, setLatestAppointments ] = useState<LatesAppointment[]>([]);
   const router = useRouter();
 
   useEffect(() => {
@@ -16,11 +18,53 @@ export default function Home() {
 
     if(sessionRole && sessionName) return;
     router.push("/login");
+
   }, [role]);
+  
+  useEffect(()=>{
+    const fetchLatestAppointments = async () =>  {
+      try{
+        const latestAppointments = await fetchBackend("appointment/currentDayBooked", "GET");
+        if(!latestAppointments.data) return;
+        setLatestAppointments(latestAppointments.data);
+      } catch (err){
+        console.error(err)
+      }
+    }
+    
+    fetchLatestAppointments();
+  }, [])
 
   return (
     <div>
-      <h1>Welcome!</h1>
+      <h1 className="text-4xl">{Greeting()}
+        <span className="text-3xl ml-4">
+          {userName}!
+        </span>
+      </h1>
+
+      <div>
+        <h2 className="text-2xl my-4">Latest Appointments</h2>
+      </div>
     </div>
   );
 }
+
+
+function Greeting():"Good Morning"|"Good Afternoon"{
+  const currentTime = new Date().getHours();
+  if(currentTime >= 12) return "Good Afternoon";
+  return "Good Morning"
+}
+
+interface LatesAppointment {
+  id: number,
+  student_id: number,
+  professor_id: number,
+  availability_id: number,
+  status: string,
+  message: string,
+  time_stamp: string,
+  created_at: string,
+  updated_at: string
+};
