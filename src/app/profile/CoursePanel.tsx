7e9af3d365ba248e1b13058682644b09091756ee
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-18 20:48:48 +0800

    handled adding/asigning course action

--- src/app/profile/CoursePanel.tsx
+++ src/app/profile/CoursePanel.tsx
@@ -3,6 +3,7 @@ import { IoMdSettings } from "react-icons/io";
 import CustomCourseDialog from "./CustomCourseDialog";
 import { BiCollapseVertical } from "react-icons/bi";
 import fetchBackend from "@/lib/fetchBackend";
+import { MdOutlineAdd } from "react-icons/md";
 
 
 export default function CoursePanel(){
@@ -10,6 +11,8 @@ export default function CoursePanel(){
 
   const [ courseList, setCourseList ] = useState<courseListItem[]>([]);
   const [ coursePickerIsOpen, setCoursePickerIsOpen ] = useState<boolean>(false);
+  const [ selectedCourseIndex, setSelectedCourseIndex ] = useState<number>(-99);
+  const selectedCourse = courseList[selectedCourseIndex];
 
   useEffect(()=>{
     const fetchCourses = async ()=>{
@@ -41,14 +44,39 @@ export default function CoursePanel(){
         <h3>Courses</h3>
       </div>
 
+      {/* shows the available course with option to use/add it */}
       <div className="[&>*]:bg-background-medium [&>*]:rounded-md">
-        <label className="flex-rl gap-2 mb-4 p-2">
-          Pick
-          <button onClick={()=>setCoursePickerIsOpen(x=>!x)}>
-            <BiCollapseVertical/>  
+        {/* contains input for adding a course */}
+        <form onSubmit={handleAddCourse}
+          className="flex-rl items-center mb-2 px-4 py-2"
+        >
+          {/* trigger button for picking a course */}
+          <label className="flex-rl gap-2 min-w-[7rem]
+            border-r-highlight-muted border-r-[1px] border-r-solid"
+          >
+            <button onClick={()=>setCoursePickerIsOpen(x=>!x)} type='button'
+              className="flex-rl items-center gap-4"  
+            >
+              <BiCollapseVertical/>
+              <p>{selectedCourse?.name}</p>
+            </button>
+          </label>
+
+          <label className="px-5 flex-1">
+            Year:
+            <select name="year">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+            </select>
+          </label>
+          {/* button for adding/using an available course */}
+          <button type='submit'>
+            <MdOutlineAdd/>
           </button>
-        </label>
-        <div className={`duration-150 [&>*]:px-4
+        </form>
+        <div className={`duration-150 [&>*]:px-4 space-y-1
           ${ coursePickerIsOpen?
             'max-h-50 overflow-y-auto'
             :'max-h-0 overflow-y-hidden'
@@ -61,10 +89,13 @@ export default function CoursePanel(){
               <p>Name</p>
               <p>Description</p>
             </div>
-            {courseList.map(course =>
-              <button className="grid grid-cols-[6rem_auto] space-x-7
-                hover:bg-background-light justify-items-start w-full"
+            {courseList.map((course, index) =>
+              <button className={`grid grid-cols-[6rem_auto] space-x-7
+                hover:bg-background-light justify-items-start w-full
+                ${selectedCourseIndex === index? 'bg-primary':''}
+                `}
                 key={course.id}
+                onClick={()=>setSelectedCourseIndex(index)}
               >
                 <p>{course.name}</p>
                 <p>{course.description}</p>
@@ -76,4 +107,32 @@ export default function CoursePanel(){
     <CustomCourseDialog ref={customCourseDialogRef}/>
     </>
   );
+
+  /**
+   * Used to assign the selected course to the logged user
+   */
+  async function handleAddCourse(e: React.SyntheticEvent<HTMLFormElement>){
+    e.preventDefault();
+    if(!selectedCourse)return;
+
+    const formData =  new FormData(e.currentTarget);
+    formData.append('course_id', `${selectedCourse.id}`);
+
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+
+    const response = await fetchBackend("course/use",{
+      method: "POST",
+      headers: { 'Content-Type':'application/json' },
+      body: JSON.stringify(data)
+    });
+
+    if (response.ok){
+      setSelectedCourseIndex(-99);
+    } else {
+      const json = await response.json() as common_response;
+      alert(json.message);
+    }
+  }
 }
