88dfa8d33eba96736958f7e8cadffdbe3c8445d3
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-18 13:13:06 +0800

    render the list of owned courses in frontend

--- src/app/profile/CoursePanel.tsx
+++ src/app/profile/CoursePanel.tsx
@@ -1,5 +1,6 @@
+import { useAuth } from "@/context/AuthContext";
 import fetchBackend from "@/lib/fetchBackend";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IoMdAddCircleOutline, IoMdCloseCircleOutline, IoMdCreate } from "react-icons/io";
 
 export default function CoursePanel(){
@@ -26,8 +27,26 @@ export default function CoursePanel(){
 function CustomCourseDialog({ref}:{
   ref: React.RefObject<HTMLDialogElement | null>
 }){
+
+  const [courseList, setCourseList] = useState<SelfCourseItem[]>([]);
+
+  useEffect(()=>{
+    const fetchCourses = async ()=>{
+      const response = await fetchBackend("course/list/self",{
+        method: "GET",
+        headers: {"Content-Type": 'application/json'}
+      });
+
+      const json = await response.json() as course_list_self_response;
+
+      if (response.ok) setCourseList(json.data);
+    }
+
+    fetchCourses();
+  }, []);
+
   return (
-    <dialog ref={ref} className="w-[30rem]">
+    <dialog ref={ref} className="w-[30rem] open:h-[70dvh]">
       <div className="flex-cr space-y-4">
         {/* close button */}
         <div className="flex-rr">
@@ -42,29 +61,46 @@ function CustomCourseDialog({ref}:{
           Create New Course
         </p>
 
-        <div className="card2 p-4">
-          <form
-            onSubmit={addCourse}
-          >
-            <div className="flex-cl gap-2">
-              <div className="flex-rl">
-                <label className="flex-1">
-                  Course Name
-                </label>
-                  <button type='submit'>
-                    <IoMdCreate/>
-                  </button>
-              </div>
-
-              <div className="flex-rc gap-2">
-                <div className="space-y-2">
-                  <input type='text' name="name" placeholder="Ex: CpE" required/>
-                  <input type='text' name="description" placeholder="Ex: Computer Engineering" required/>
-                </div>
-              </div>
+        <form
+          className="card2 py-4 px-8"
+          onSubmit={addCourse}
+        >
+          <div className="flex-cl gap-2">
+            <div className="flex-rl">
+              <label className="flex-1">
+                Course Name
+              </label>
+                <button type='submit'>
+                  <IoMdCreate/>
+                </button>
             </div>
 
-          </form>
+            <div className="flex-rc gap-2">
+              <div className="space-y-2">
+                <input type='text' name="name" placeholder="Ex: CpE" required/>
+                <input type='text' name="description" placeholder="Ex: Computer Engineering" required/>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <div className="card2 px-8 py-4">
+          <p>Your Courses Created</p>
+
+          <div className="py-4 px-6 bg-background-medium flex-cc gap-4 rounded-md">
+          {
+            courseList?.map(course =>
+              <div className="grid grid-cols-[max-content_auto] grid-flow-row
+                space-x-4 border-b-highlight-muted border-b-[1px] border-b-solid
+                pb-2 px-4"
+                key={course.id}
+              >
+                <p>Name: </p> <p>{course.name}</p>
+                <p>Description:</p> <p>{course.description}</p>
+              </div>
+            )
+          }
+          </div>
         </div>
       </div>
     </dialog>
@@ -89,11 +125,17 @@ function CustomCourseDialog({ref}:{
     if (response.ok && json.success) {
       const newId = json.data.id;
 
+      const newCourse: SelfCourseItem = {
+        id: newId,
+        description: `${data.description}`,
+        name: `${data.name}`
+      }
+
+      setCourseList(x => [...x, newCourse]);
+      
       form.reset();
     } else {
       alert(json.message);
     };
-
-
   }
 }
--- types/api/course.d.ts
+++ types/api/course.d.ts
@@ -4,3 +4,22 @@ interface course_create_response extends common_response {
         id: number;
     }
 }
+
+interface course_list_response extends common_response {
+    data: {
+        id: number;
+        created_by: string;
+        name: string;
+        description: string;
+    }[]
+}
+
+interface SelfCourseItem {
+  id: number;
+  name: string;
+  description: string;
+}
+
+interface course_list_self_response extends common_response {
+    data: SelfCourseItem[]
+}
