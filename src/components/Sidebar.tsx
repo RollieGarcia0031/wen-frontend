a9f9a6ce24823cf25101d3550a55ec19b9432649
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-30 21:39:05 +0800

    fix sidebar rendering conditionally

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,20 @@
 "use client"
 
 import { HiOutlineHome } from "react-icons/hi2";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Sidebar(){
+  const path = usePathname();
+  
+  if (path.includes("/login")){
+    return <span> </span>; 
+  }
+
   return (
     <div
-      className="flex-rt p-4
+      className="flex-rt
+        py-4
         [&_svg]:text-2xl
         [&_button]:bg-black [&_button]:p-3 [&_button]:rounded-full
         border-r-highlight-muted border-r-[1px]
