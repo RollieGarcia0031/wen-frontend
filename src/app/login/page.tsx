50406ef301408b5de8bae23e129e368eec15bd58
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-31 13:23:40 +0800

    add animation for switching log option

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,18 +14,28 @@ export default function Login(){
   return (
     <div className='flex-full-center min-h-screen'>
       <main
-        className='flex-rc card
-        px-5 py-15 gap-15'
+        className={`card w-[60rem]
+        ${option == 'signup'? 'h-[35rem]': 'h-[25rem]'}
+        duration-700 overflow-y-hidden
+        grid grid-cols-[25rem_auto]
+        px-5 py-10 gap-15`}
       >
         <AuthForm option={option} setOption={setOption}/>
-        <Image src='/student-teacher.svg' width={400} height={400} alt='student-teacher'/>
+        <div className='flex-full-center h-full'>
+          <Image src='/student-teacher.svg' width={400} height={400} alt='student-teacher'/>
+        </div>
       </main>
     </div>
   );
 }
 
+/**
+ * Conditional component that renders the login/signup form based on the option state
+ */ 
 function AuthForm({option, setOption}: {
+  /** The state that serves as base condition on which  to render*/
   option: logOption,
+  /** To change the current option state */
   setOption: React.Dispatch<React.SetStateAction<logOption>>
 }){
   switch (option) {
