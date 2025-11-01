f85606176aca3c777f43f9540546fab4d9f8deae
RollieGarcia0031 <garciarollie827@gmail.com>
2025-11-01 20:36:15 +0800

    create context for appointment

--- src/app/appointment/student/page.tsx
+++ src/app/appointment/student/page.tsx
@@ -1,9 +1,31 @@
 "use client"
 
+import { FaPlus } from "react-icons/fa";
+import { useAppointment } from "@/context/AppointmentContext";
+
 export default function Student(){
+
+  const { setSearchDialogOpened } = useAppointment();
+
   return (
-    <div>
-      student
+    <div className="px-10 mt-4">
+
+      <div className="flex-rl">
+        <h1 className="flex-1">
+          Sent Appointments
+        </h1>
+
+        <button
+          className="bg-primary hover:bg-primary-hover px-4 rounded-md
+            shadow-black shadow-md
+          "
+          title="create new appointment"
+          onClick={()=>setSearchDialogOpened(true)}
+        >
+          <FaPlus />
+        </button>
+
+      </div>
     </div>
 
   );
---
+++ src/context/AppointmentContext.tsx
@@ -0,0 +1,37 @@
+import { useContext, createContext, useState, Dispatch, SetStateAction } from "react";
+
+interface AppointmentProps {
+  /**
+   * State for UI to determine weather to render
+   * the search dialog
+   */
+  searchDialogOpened: boolean;
+  /**
+   * Control the visilibility of search dialog
+   * to search for professors
+   */
+  setSearchDialogOpened: Dispatch<SetStateAction<boolean>>
+}
+
+const AppointmentContext = createContext<AppointmentProps>({
+  searchDialogOpened: false,
+  setSearchDialogOpened: (arg) => arg
+});
+
+export function AppointmentContextProvider({children}: {
+  children: React.ReactNode
+}){
+
+  const [ searchDialogOpened, setSearchDialogOpened ] = useState(false);
+
+  return (
+    <AppointmentContext.Provider
+      value={{ setSearchDialogOpened, searchDialogOpened }}
+    >
+      {children}
+    </AppointmentContext.Provider>
+  );
+
+}
+
+export const useAppointment = () => useContext(AppointmentContext);
