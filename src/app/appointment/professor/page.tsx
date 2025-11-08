e472d0cd3d1842e515a35dc1f9f0169c5ade02ad
RollieGarcia0031 <garciarollie827@gmail.com>
2025-11-08 19:44:26 +0800

    add card component

--- src/app/appointment/professor/page.tsx
+++ src/app/appointment/professor/page.tsx
@@ -1,11 +1,58 @@
 "use client"
 
+import { fetchRecievedAppointments, ProfAppointmentContextProvider, useProfAppointment } from "@/context/ProfessorAppointmentContext";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect } from "react";
+
 export default function Professor(){
   return (
+    <ProfAppointmentContextProvider>
+      <div>
+
+        Proffessor
+
+        <AppointmentTable />
+
+      </div>
+    </ProfAppointmentContextProvider>
+  );
+}
+
+/**
+ * Table containing the recived appointments of the user
+ */
+function AppointmentTable(){
+
+  const { setRecievedAppointments, recievedAppointments } = useProfAppointment();
+
+  useEffect(() => {
+    fetchRecievedAppointments(setRecievedAppointments);
+  }, []);
+
+  return (
     <div>
-
-      Proffessor
-
+      <AnimatePresence>
+        {recievedAppointments.map(item => (
+          <AppointmentCard key={item.id} item={item} />
+        ))}
+      </AnimatePresence>
     </div>
+  );
+}
+
+function AppointmentCard({item}: {
+  item: appointment_list_response_item
+}) {
+
+  const { name, status, day_of_week } = item;
+
+  return (
+    <motion.div>
+
+      <p>
+        {name}
+      </p>
+
+    </motion.div>
   );
 }
