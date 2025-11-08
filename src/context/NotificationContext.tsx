9cfb2e35dd39b1b6156f41e46169fc0a1310ff6f
RollieGarcia0031 <garciarollie827@gmail.com>
2025-11-08 10:54:51 +0800

    add function for fetching notifications

--- src/context/NotificationContext.tsx
+++ src/context/NotificationContext.tsx
@@ -9,17 +9,26 @@ interface NotificationContextProps {
    */
   notifications: notification_list_all_response_item[];
   setNotifications: Dispatch<SetStateAction<notification_list_all_response_item[]>>;
+  /**
+   * The id of next notification to fetch,
+   *  - will be used for pagination
+   */
+  notifNextCursor: number;
+  setNotifNextCursor: Dispatch<SetStateAction<number>>;
 
   /**
    * List of unseen notifications
    */
   unreadNotifications: notification_list_unread_response_item[];
-  setUnreadNotifications: Dispatch<SetStateAction<notification_list_unread_response_item[]>>
+  setUnreadNotifications: Dispatch<SetStateAction<notification_list_unread_response_item[]>>;
 }
 
 const Context = createContext<NotificationContextProps>({
   notifications: [],
   setNotifications: ()=>{},
+
+  notifNextCursor: 0,
+  setNotifNextCursor: ()=>{},
 
   unreadNotifications: [],
   setUnreadNotifications: ()=>{}
@@ -28,6 +37,7 @@ const Context = createContext<NotificationContextProps>({
 export function NotificationContextProvider({children}:{
   children: React.ReactNode
 }) {
+  const [ notifNextCursor, setNotifNextCursor ] = useState(0);
   const [ notifications, setNotifications ] = useState<notification_list_all_response_item[]>([]);
   const [ unreadNotifications, setUnreadNotifications ] = useState<notification_list_unread_response_item[]>([]);
 
@@ -40,7 +50,9 @@ export function NotificationContextProvider({children}:{
       notifications,
       setNotifications,
       unreadNotifications,
-      setUnreadNotifications
+      setUnreadNotifications,
+      notifNextCursor,
+      setNotifNextCursor
     }}>
       {children}
     </Context.Provider>
@@ -70,6 +82,39 @@ export async function refreshUnreadNotifications(
   const { data } = await response.json() as notification_list_unread_response;
   
   setUnreadNotifications(data);
+}
+
+/**
+ * fetches the list of notification with pagination
+ *
+ * @param setNotifications - hook for updated
+ * @param setNotifNextCursor - hook for pagination
+ * @param end_from - derived from next_cursor of this response
+ *                 - for first time api call, it should be zero (0)
+ */
+export async function fetchNotifications(
+  setNotifications: Dispatch<SetStateAction<notification_list_all_response_item[]>>,
+  setNotifNextCursor: Dispatch<SetStateAction<number>>,
+  end_from: number
+) {
+  const reqBody = { end_from };
+
+  const response = await fetchBackend("notification/list/all", {
+    method: "POST",
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify(reqBody)
+  });
+
+  if (!response.ok) {
+    return;
+  }
+
+  const { data } = await response.json() as notification_list_all_response;
+
+  setNotifications(data.data);
+  setNotifNextCursor(data.next_cursor);
+
+  return data.next_cursor;
 }
 
 export const useNotification = () => useContext(Context);
