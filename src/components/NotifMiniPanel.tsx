311ad9fcafd9a928d418b07cf3ad51d446306302
RollieGarcia0031 <garciarollie827@gmail.com>
2025-11-08 12:22:03 +0800

    render unread notif for all users

--- src/components/NotifMiniPanel.tsx
+++ src/components/NotifMiniPanel.tsx
@@ -1,17 +1,57 @@
 "use client"
 
+import { useNotification } from "@/context/NotificationContext";
 import { RefObject } from "react";
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function NotifMiniPanel({ref}:{
   ref: RefObject<HTMLDivElement | null>
 }) {
+
+  const { unreadNotifications } = useNotification();
+
   return (
     <div
       ref={ref}
       className="card p-4 absolute right-[5rem]
       w-[20rem]"
     >
-      Notif
+      <AnimatePresence>
+        {
+          unreadNotifications.map(item => (
+            <NotifCard key={item.notification_id} item={item} />
+          ))
+        }
+      </AnimatePresence>
     </div>
+  );
+}
+
+function NotifCard({item}:{
+  item: notification_list_unread_response_item
+}){
+  
+  const { id, message, created_at } = item;
+
+  const displayDate = new Date(created_at).toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  });
+
+  return (
+    <motion.div
+      className="card2 text-sm"
+    >
+      <p>
+        {message}
+      </p>
+      
+      <p className="text-text-muted border-t-[1px] border-t-solid border-t-highlight-muted
+        mt-2"
+      >
+        {displayDate}
+      </p>
+    </motion.div>
   );
 }
