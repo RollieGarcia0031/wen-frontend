286843075e415b8d0858bda4bd97c49aa52626b2
RollieGarcia0031 <garciarollie827@gmail.com>
2025-11-08 10:35:56 +0800

    add refresh unread notif function

--- src/context/NotificationContext.tsx
+++ src/context/NotificationContext.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import fetchBackend from "@/lib/fetchBackend";
 import { createContext, useContext, useState, useEffect, Dispatch, SetStateAction } from "react";
 
 interface NotificationContextProps {
@@ -30,6 +31,10 @@ export function NotificationContextProvider({children}:{
   const [ notifications, setNotifications ] = useState<notification_list_all_response_item[]>([]);
   const [ unreadNotifications, setUnreadNotifications ] = useState<notification_list_unread_response_item[]>([]);
 
+  useEffect(()=>{
+    refreshUnreadNotifications(setUnreadNotifications);
+  }, [])
+
   return (
     <Context.Provider value={{
       notifications,
@@ -40,6 +45,31 @@ export function NotificationContextProvider({children}:{
       {children}
     </Context.Provider>
   );
+}
+
+/**
+ * Refresh the list of unread notifications
+ */
+export async function refreshUnreadNotifications(
+  setUnreadNotifications: Dispatch<SetStateAction<notification_list_unread_response_item[]>>
+) {
+  const reqBody = {
+    end_from: 0
+  };
+
+  const response = await fetchBackend("notification/list/unread", {
+    method: "POST",
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify(reqBody)
+  });
+
+  if (!response.ok){
+    return;
+  }
+
+  const { data } = await response.json() as notification_list_unread_response;
+  
+  setUnreadNotifications(data);
 }
 
 export const useNotification = () => useContext(Context);
