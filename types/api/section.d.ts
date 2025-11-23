5ce6a68bf4b5f6b90b6ddef703f7a8226bffb6d8
RollieGarcia0031 <garciarollie827@gmail.com>
2025-11-23 19:24:03 +0800

    add interface for get response

---
+++ types/api/section.d.ts
@@ -0,0 +1,15 @@
+interface section_item {
+    section_id: number;
+    year_level: number;
+    section_code: string;
+}
+
+interface section_list_all_response_item {
+    course_name: string;
+    course_code: string;
+    sections: section_item[];
+}
+
+interface section_list_all_response extends common_response {
+    data: section_list_all_response_item[]
+}
