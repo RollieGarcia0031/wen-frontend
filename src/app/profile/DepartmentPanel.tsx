b8c2170669906c039f1994e3cf7881b65fe6b805
RollieGarcia0031 <garciarollie827@gmail.com>
2025-11-24 14:05:00 +0800

    create temporary department card

--- src/app/profile/DepartmentPanel.tsx
+++ src/app/profile/DepartmentPanel.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import fetchBackend from "@/lib/fetchBackend";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { IoRemoveCircleOutline } from "react-icons/io5";
 import { toast } from "react-toastify";
 
 export default function DepartmentPanel(){
@@ -9,8 +10,12 @@ export default function DepartmentPanel(){
   /**
    * The department that the user has joined
    */
-  const [ joinedDepartment, setJoinedDepartment ] = useState<department_list_all_response_item[]>();
-  const [ allDepartments, setAllDepartments ] = useState<department_list_all_response_item[]>();
+  const [ joinedDepartment, setJoinedDepartment ] = useState<department_list_all_response_item[]>([]);
+  const [ allDepartments, setAllDepartments ] = useState<department_list_all_response_item[]>([]);
+  /**
+   * Ids of the departments to be added
+   */
+  const [ temporaryDepartments, setTemporaryDepartments ] = useState<number[]>([]);
 
   useEffect(()=>{
     fetchJoinedDepartment();
@@ -41,10 +46,25 @@ export default function DepartmentPanel(){
         </div>
 
         <button
+          onClick={()=>setTemporaryDepartments(x=>[...x, 0])}
           className="bg-white text-black py-1 px-3 rounded-md mt-4"
         >
           Join a department
         </button>
+
+        <div
+          className="mt-4 px-10"
+        >
+          {
+            temporaryDepartments.map((id, index)=>
+              <TemporaryDepartmentCard key={index} index={index}
+                allDepartments={allDepartments}
+                setTemporaryDepartments={setTemporaryDepartments}
+                temporaryDepartments={temporaryDepartments}
+              />
+            )
+          }
+        </div>
       </div>
     </div>
   );
@@ -138,5 +158,53 @@ function DepartmentCard({item, refreshJoinedDepartment}:{
     } finally {
       refreshJoinedDepartment();
     }
+  }
+}
+
+function TemporaryDepartmentCard({index, allDepartments, setTemporaryDepartments, temporaryDepartments}:{
+  index: number,
+  allDepartments: department_list_all_response_item[],
+  setTemporaryDepartments: Dispatch<SetStateAction<number[]>>,
+  temporaryDepartments: number[]
+}){
+
+  if (index >= allDepartments.length)
+    return null;
+
+  return (
+    <div
+      className="my-2 grid grid-cols-[1fr_auto] items-center justify-center gap-5"
+    >
+      <select
+        value={temporaryDepartments[index]}
+        onChange={e => handleChange(e)}
+      >
+        {
+          allDepartments.map((item, index)=>
+            <option key={index} value={item.id}>
+              {`${item.code}   -   ${item.name}`}
+            </option>
+          )
+        }
+      </select>
+
+      <button
+        onClick={handleRemove}
+      >
+        <IoRemoveCircleOutline className="text-xl" />
+      </button>
+    </div>
+  );
+
+  function handleChange(event: React.ChangeEvent<HTMLSelectElement>){
+    setTemporaryDepartments(prev => {
+      const newTemporaryDepartments = [...prev];
+      newTemporaryDepartments[index] = Number(event.target.value);
+      return newTemporaryDepartments;
+    });
+  }
+
+  function handleRemove(){
+    setTemporaryDepartments(prev => prev.filter((_, i) => i !== index));
   }
 }
